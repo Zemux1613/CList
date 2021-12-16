e2ac298dc61db137d9ad755b7fa8e6782e8d2a04
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2021-12-16 11:34:17 +0100

    feat: add testing logic for printNodes method

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,9 @@ int main() {
         addNode(i, firstNode);
         printf("add node %d\n", i);
     }
+    
+    printf("\nAll Nodes:\n");
+    printNodes(firstNode);
 
     printf("\nNode6:\n");
 
